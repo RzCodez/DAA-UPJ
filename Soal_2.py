04cb04bb3653cfdb0fc11f7996ffe3d384e9c5e1
_userNull <89193581+RzCodez@users.noreply.github.com>
2023-08-30 13:51:18 +0700

    Update Soal_2.py

--- Soal_2.py
+++ Soal_2.py
@@ -2,5 +2,18 @@
 # Program Studi: Informatika
 # Mata Kuliah: DAA (INF-A)
 
-# 1. Cari nilai KPK dari 3 dan 4
+# 2. Fungsi untuk menukar posisi dua variabel x dan y, dengan kasus : 
+# Ada 2 buah: manggis dan pisang. Manggis di piring 1, Pisang di piring 2. Piring 3 kosong.
+
+piring_1 = "Manggis"
+piring_2 = "Pisang"
+piring_3 = None
+
+temp = piring_1
+piring_1 = piring_2
+piring_2 = temp
+
+print("piring 1 berisikan: ", piring_1)
+print("piring 2 berisikan: ", piring_2)
+print("piring 3 berisikan: ", piring_3)
 
