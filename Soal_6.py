0d91b27afb02c48789480005544dd599ee02aff1
_userNull <89193581+RzCodez@users.noreply.github.com>
2023-08-30 14:09:44 +0700

    Create Soal_6.py

---
+++ Soal_6.py
@@ -0,0 +1,18 @@
+"""
+Nama: Kafka Ramadityo
+Program Studi: Informatika
+MatKul: DAA (INF-A)
+
+
+6. Tentukan algoritma dan pseudocode volume kerucut
+(diameter = 5, tinggi = 4) 
+"""
+
+diameter = 5
+jariJari = diameter / 2
+tinggi = 4
+pi = 3.14
+
+volumeKerucut = 1/3 * pi * (jariJari ** 2) * tinggi
+
+print("Volume kerucut: ", volumeKerucut)
