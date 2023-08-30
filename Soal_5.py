799fa4b63d454da656b4b9088e22a9ad75f6ffc1
_userNull <89193581+RzCodez@users.noreply.github.com>
2023-08-30 14:06:05 +0700

    Create Soal_5.py

---
+++ Soal_5.py
@@ -0,0 +1,11 @@
+"""
+5. Tentukan algoritma dan pseudocode volume tabung (jari-jari = 3, tinggi = 5)
+"""
+
+pi = 3.14
+jari = 3
+tinggi = 5
+
+volumeTabung = pi * (jari**2) * tinggi
+
+print("Volume tabung: ", volumeTabung)
