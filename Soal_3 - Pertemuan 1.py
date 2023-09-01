091751633e77cbf65dda51c70c5f7a455e46f067
Kafka Ramadityo <89193581+RzCodez@users.noreply.github.com>
2023-09-01 15:29:44 +0700

    Update Soal_3 - Pertemuan 1.py

--- Soal_3 - Pertemuan 1.py
+++ Soal_3 - Pertemuan 1.py
@@ -1,4 +1,5 @@
 """
+
 Nama: Kafka Ramadityo
 Program Studi: Informatika
 Mata Kuliah: DAA (INF-A)
@@ -7,6 +8,24 @@ Mata Kuliah: DAA (INF-A)
 Alas 25
 Tinggi 30
 Hitung luas dari segitiga tersebut.
+
+#Algoritma 
+
+1. Deklarasikan variabel alas dengan nilai 25. 
+2. Deklarasikan variabel tinggi dengan nilai 30. 
+3. Hitung luas segitiga dengan rumus: luas = 1/2 * alas * tinggi. 
+4. Tampilkan nilai alas dan tinggi. 
+5. Tampilkan nilai luas segitiga. 
+
+#Pseudocode 
+
+SET alas TO 25 
+SET tinggi TO 30 
+SET luas TO 0.5 * alas * tinggi 
+
+DISPLAY "alas: " + alas + " tinggi: " + tinggi 
+DISPLAY "Luas dari segitiga adalah: " + luas 
+
 """
 
 alas = 25
